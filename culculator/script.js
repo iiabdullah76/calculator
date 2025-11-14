9a444d48cab881fdd9ce794ead5a32a9197b9c63
Abdullah <abdullah.angari4@gmail.com>
2025-11-14 23:14:23 +0300

    feat: add calculator and tic-tac-toe (HTML/CSS/JS)

---
+++ culculator/script.js
@@ -0,0 +1,33 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function appendOperator(operator) {
+    if (displayValue !== '' && !isNaN(displayValue.charAt(displayValue.length - 1))) {
+        displayValue += operator;
+        document.getElementById('display').value = displayValue;
+    }
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function backspace() {
+    displayValue = displayValue.slice(0, -1);
+    document.getElementById('display').value = displayValue;
+}
+
+function calculate() {
+    try {
+        const result = eval(displayValue);
+        displayValue = result.toString();
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        alert('Invalid expression');
+    }
+}
